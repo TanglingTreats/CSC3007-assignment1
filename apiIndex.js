4ff7e9f86f437a9c0024cc945e644f001e58ec41
Edwin Tang <edwintang10@gmail.com>
2022-05-26 22:40:25 +0800

    Change query method for api from ajax to fetch. Add table in index. Add functions to parse data from api.

--- apiIndex.js
+++ apiIndex.js
@@ -1,14 +1,51 @@
-var url = "https://api.data.gov.sg/v1/environment/psi";
+var apiUrl = "https://api.data.gov.sg/v1/environment/psi";
 
-function loadPage() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function () {
-    const res = xhttp.response;
-    console.log(res);
-  };
+var updatedTime = document.getElementById("updatedTime");
+var tBody = document.getElementById("tableBody");
 
-  xhttp.open("GET", url, true);
-  xhttp.send();
+async function getUrl(url) {
+  const response = await fetch(url);
+
+  var data = await response.json();
+  return data;
 }
 
-loadPage();
+function formatDate(elem, date) {
+  const options = {
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false,
+  };
+  const formattedDate = date.toLocaleDateString("en-SG", options);
+  elem.textContent = `${formattedDate}`;
+}
+
+function formatData(elem, dataBlock) {
+  for (data in dataBlock) {
+    const row = tBody.insertRow();
+    const cellName = row.insertCell();
+    const cellNational = row.insertCell();
+    const cellNorth = row.insertCell();
+    const cellSouth = row.insertCell();
+    const cellEast = row.insertCell();
+    const cellWest = row.insertCell();
+    const cellCentral = row.insertCell();
+
+    cellName.innerHTML = `${data}`;
+    cellNational.innerHTML = `${dataBlock[data].national}`;
+    cellNorth.innerHTML = `${dataBlock[data].north}`;
+    cellSouth.innerHTML = `${dataBlock[data].south}`;
+    cellEast.innerHTML = `${dataBlock[data].east}`;
+    cellWest.innerHTML = `${dataBlock[data].west}`;
+    cellCentral.innerHTML = `${dataBlock[data].central}`;
+  }
+}
+
+getUrl(apiUrl).then((data) => {
+  formatDate(updatedTime, new Date(data.items[0].update_timestamp));
+  formatData(tBody, data.items[0].readings);
+});
